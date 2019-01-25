fe4f1320e9b8fe9836a172b1a3357039cbbe1615
Mohit Singh <mohitsingh1997@gmail.com>
2019-01-25 18:46:54 +0530

    Imports added

---
+++ main.py
@@ -0,0 +1,15 @@
+from PyQt4 import QtGui
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+import sys
+# Importing modules required for PyQt
+from facepy import GraphAPI
+import json
+from json2html import *
+import webbrowser
+
+#Importing the modules for the script
+import experiment
+#Importing the design module
+#################################################################
+
