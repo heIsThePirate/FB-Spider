93963dfbee72a137c9a59ca04a64d58213a52c95
mohit singh <mohitsingh1997@gmail.com>
2019-03-21 08:57:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name ='FB-Spider',
     version ='2.0',
     description = 'accepts the id of a Facebook page and transforms into a table of the latest 5 posts and the respective latest 5 comments per post. The table will be in .html format',
-    author = 'Parth Verma',
-    author_email = 'vermaparth97@gmail.com',
+    author = 'Mohit Singh',
+    author_email = 'mohitsingh1997@gmail.com',
     packages = find_packages(),
     include_package_data = True,
     install_requires=[
